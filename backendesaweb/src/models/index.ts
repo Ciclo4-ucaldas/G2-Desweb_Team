70d52cd2896da95990c5dae584ac59f0a825b75b
thunderthard <hars1302@gmail.com>
2022-11-08 17:21:20 -0500

    creacion del modelo menu

--- backendesaweb/src/models/index.ts
+++ backendesaweb/src/models/index.ts
@@ -1,3 +1,4 @@
 export * from './persona.model';
 export * from './administrador.model';
 export * from './restaurante.model';
+export * from './menu.model';
---
+++ backendesaweb/src/models/menu.model.ts
@@ -0,0 +1,34 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Menu extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  tipoMenu: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Precio: string;
+
+
+  constructor(data?: Partial<Menu>) {
+    super(data);
+  }
+}
+
+export interface MenuRelations {
+  // describe navigational properties here
+}
+
+export type MenuWithRelations = Menu & MenuRelations;
