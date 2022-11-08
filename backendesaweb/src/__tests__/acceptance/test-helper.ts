d92efc5bc344d6bc075a0c46aa4d91487fb10510
thunderthard <hars1302@gmail.com>
2022-11-08 17:09:33 -0500

    Creacion proyecto loopback

---
+++ backendesaweb/src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {BackendesawebApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new BackendesawebApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: BackendesawebApplication;
+  client: Client;
+}
