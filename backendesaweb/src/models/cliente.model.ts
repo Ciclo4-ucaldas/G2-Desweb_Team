50f56bd30cbd9f3dcd139f0a473dc7d9b4406b2d
Lperezn <liperez033@gmail.com>
2022-11-08 20:54:50 -0500

    Creacion de modelos cliente, pedido y producto

---
+++ backendesaweb/src/models/cliente.model.ts
@@ -0,0 +1,41 @@
+import {model, property} from '@loopback/repository';
+import {Persona} from '.';
+
+@model()
+export class Cliente extends Persona {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  Id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Direccion: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Correo: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Celular: string;
+
+
+  constructor(data?: Partial<Cliente>) {
+    super(data);
+  }
+}
+
+export interface ClienteRelations {
+  // describe navigational properties here
+}
+
+export type ClienteWithRelations = Cliente & ClienteRelations;
---
+++ backendesaweb/src/models/pedido.model.ts
@@ -0,0 +1,46 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Pedido extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  Id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Menu: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  Cantidad: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  Total_Pago: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Estado: string;
+
+
+  constructor(data?: Partial<Pedido>) {
+    super(data);
+  }
+}
+
+export interface PedidoRelations {
+  // describe navigational properties here
+}
+
+export type PedidoWithRelations = Pedido & PedidoRelations;
